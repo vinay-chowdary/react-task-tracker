50a6c08a0e25b2af9d801d8578ecc0c441a68ad7
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-01-20 00:38:23 +0530

    add button with form

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,47 @@
-import PropTypes from 'prop-types'
+import { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { TaskContext } from './TaskContext';
+
 
 function Button({ color, text, onClick }) {
+
+
+    const [tasks, setTasks] = useContext(TaskContext);
+    const nextId = tasks.length + 1;
+    const [newTask, setNewTask] = useState({
+        id: nextId,
+        text: ""
+    })
+
+
+    const handleClick = () => {
+        setTasks((prevTasks) => [...prevTasks, newTask])
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewTask((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+
     return (
-        <button className="btn" style={{
-            backgroundColor: { color },
-        }}
-            onClick={onClick}
-        >{text}</button>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            setNewTask({
+                id: nextId,
+                text: ""
+            })
+        }}>
+            <input type="text" name="text" value={newTask.text} onChange={handleChange} />
+            <button className="btn" style={{
+                backgroundColor: { color },
+            }}
+                onClick={handleClick}
+            >{text}</button>
+        </form>
     )
 }
 
